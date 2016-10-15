cd761f9d7e29717e2fd842dd313155d5c92bfba5
Wes Reid <bwreid@gmail.com>
2016-10-15 15:45:17 -0600

    Restructure auth.js

--- src/server/routes/auth.js
+++ src/server/routes/auth.js
@@ -2,11 +2,16 @@ const express = require('express')
 const router = express.Router()
 const api = require('./api')
 
-router.get('/signin', (req, res, next) => {
-  res.render('auth/index', {})
-})
+router.get('/signin', index)
+router.post('/signin', signin)
 
-router.post('/signin', (req, res, next) => {
+////
+
+function index (req, res, next) {
+  res.render('auth/index', {})
+}
+
+function signin (req, res, next) {
   api.signin(req.body)
     .then(auth => {
       res.status(201).json(auth)
@@ -15,6 +20,6 @@ router.post('/signin', (req, res, next) => {
       req.flash('error', 'Could not login with the provided email and password')
       res.redirect('/signin#signin')
     })
-})
+}
 
 module.exports = router
