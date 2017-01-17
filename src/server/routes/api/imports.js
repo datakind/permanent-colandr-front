ae03306b19efe12e231132f84f4102b07796a891
Sam Anzaroot <sam@samanz.com>
2017-01-16 21:40:29 -0500

    Import

--- src/server/routes/api/imports.js
+++ src/server/routes/api/imports.js
@@ -10,7 +10,6 @@ function create (body, { uploaded_file: [file] }) {
   const opts = { method: 'POST' }
   let request = send(`/citations/imports`, user, opts)
   let form = request.form()
-
   form.append('uploaded_file', file.buffer.toString(), {
     filename: file.originalname,
     contentType: file.mimetype
@@ -18,7 +17,9 @@ function create (body, { uploaded_file: [file] }) {
   form.append('review_id', reviewId)
   form.append('source_type', body.source_type)
   form.append('source_name', body.source_name)
-  form.append('source_url', body.source_url)
+  if (body.source_url.length > 0) {
+    form.append('source_url', body.source_url)
+  }
   form.append('status', body.status)
 
   return request
