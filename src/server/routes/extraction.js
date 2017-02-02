57a48f4a86d4073a82726a1e427134180ff3db45
Stan S <ysagal+git@gmail.com>
2017-02-02 12:48:05 -0500

    Minor changes to address feedback
    - Cleaner mapping for data_extraction count names
    - Used nunjucks template inheritance for extraction-listing

--- src/server/routes/extraction.js
+++ src/server/routes/extraction.js
@@ -1,19 +1,13 @@
 const bluebird = require('bluebird')
-// const _ = require('lodash')
 const router = require('express-promise-router')({ mergeParams: true })
 const api = require('./api')
 const { send } = require('./api/helpers')
-// const keyterms = require('./keyterms')
 
 router.get('/', api.populateBodyWithDefaults, render)
 router.get('/:status', api.populateBodyWithDefaults, render)
 router.get('/:status/:page', api.populateBodyWithDefaults, render)
 
-const routeMap = {
-  'not_started': 'not_started',
-  'incomplete': 'started',
-  'complete': 'finished'
-}
+const kResultsPerPage = 10
 
 function render (req, res) {
   console.warn('routes/extraction/render')
@@ -28,20 +22,21 @@ function render (req, res) {
       review_id: reviewId,
       data_extraction_status: status,
       page,
-      per_page: 10
+      per_page: kResultsPerPage
     } }),
     (progress, studies) => {
-      let numPages = Math.ceil(progress.data_extraction[routeMap[status]] / 10) || 1
+      let counts = {
+        not_started: progress.data_extraction.not_started,
+        incomplete: progress.data_extraction.started,
+        complete: progress.data_extraction.finished
+      }
+      let numPages = Math.ceil(counts[status] / kResultsPerPage) || 1
       res.render('extraction/index', {
         reviewId,
         status,
-        counts: {
-          not_started: progress.data_extraction.not_started,
-          incomplete: progress.data_extraction.started,
-          complete: progress.data_extraction.finished
-        },
         studies,
         page,
+        counts,
         numPages
       })
     })
